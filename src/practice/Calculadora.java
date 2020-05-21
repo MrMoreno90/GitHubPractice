d02da7baa2b3d2617f41e590b9af4b56dddfbb44
adrian <adrian@MSI>
2020-05-21 17:49:36 +0100

    Primer commit--------;

---
+++ src/practice/Calculadora.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Calculadora {
+	private int a;
+	private int b;
+	private int c;
+	
+	public int suma (int n1,int n2) {
+	  int n3;
+	  n3=n1+n2;
+	  return n3;
+	}
+}
---
+++ src/practice/principal.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class principal {
+
+	public static void main(String[] args) {
+		Calculadora c= new Calculadora();
+        int resul = c.suma(2, 3);
+        System.out.println(resul);
+	}
+
+}
