ed61ebb08f2e9cb8a905a1556027dbdc1d140d20
adrian <adrian@MSI>
2020-05-21 19:50:19 +0100

    Haciendo commit de resta en Branch1

--- src/practice/Calculadora.java
+++ src/practice/Calculadora.java
@@ -10,4 +10,9 @@ public class Calculadora {
 	  n3=n1+n2;
 	  return n3;
 	}
+	public int resta (int n1, int n2) {
+		int n3;
+		n3=n1-n2;
+		return n3;
+	}
 }
---
+++ src/practice/suma.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class suma {
+
+	public static void main(String[] args) {
+		Calculadora c= new Calculadora();
+        int resulSuma = c.suma(2, 3);
+        System.out.println("la suma es "+resulSuma);
+        
+	}
+
+}
